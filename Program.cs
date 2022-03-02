74ca2ce7e2995642066ea149c517dac6c0b597dd
Tobias Fischer <61592216+tobi187@users.noreply.github.com>
2022-03-02 10:11:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+using System.IO;
+using PowerArgs;
+
+namespace sftpTest
+{
+    [TabCompletion]
+    [ArgExceptionBehavior(ArgExceptionPolicy.StandardExceptionHandling)]
+    public class ParseArgs
+    {
+        [ArgShortcut(ArgShortcutPolicy.NoShortcut),
+            ArgRequired,
+            ArgDescription("Adresse des Server"),
+            ArgPosition(0)]
+        public string? Server { get; set; }
+
+        [ArgRequired(PromptIfMissing =true),
+            ArgShortcut("-x")]
+        public string? Protokoll { get; set; }
+        [ArgRequired]
+        public string path { get; set; }
+
+        [ArgRequired]
+        [ArgShortcut("-s")]
+        public string Pattern { get; set; }
+        [ArgShortcut("-a")]
+        public string BasicAuth { get; set; }
+        [ArgShortcut("-k")]
+        public string KeyFile { get; set; }
+        public int Port { get; set; }
+        public bool Test { get; set; } 
+
+    }
+
+    public static class SftpTests
+    {
+        static void Main(string[] args)
+        {
+            /*ParseArgs command;
+            try
+            {
+                command = Args.Parse<ParseArgs>(args);
+            }
+            catch (ArgException e) { throw new ArgException(e.Message); }*/
+
+            sftpConnection connection = new sftpConnection();
+            /*if (!connection.areArgsValid(command))
+                return;*/
+
+            connection.DoStuff("txt", "tobi:nikelef123", "127.0.0.1", true);
+
+        }
+    }
+}
---
+++ sftpConnection.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+using System.IO;
+
+
+namespace sftpTest
+{
+    internal class sftpConnection
+    {
+        private readonly string _host = "127.0.0.1";
+        private readonly string _userName = "tobi";
+        private readonly string _password = "nikelef123";
+
+        internal void DoStuff(string ending, string auth, string host, bool isTest)
+        {
+            string[] authData = auth.Split(":");
+            string userName = authData[0];
+            string password = authData[1];
+
+            using (SftpClient client = new SftpClient(host, userName, password))
+            {
+                try
+                {
+                    client.Connect();
+                    var files = client.ListDirectory("/TestData/");
+                    foreach (var file in files) {
+                        var parts = file.Name.Split(".");
+                        if (parts.Last() == ending)
+                        {
+                            var currDate = DateTime.Now.AddDays(-3);
+                            if (file.LastWriteTime < currDate)
+                            {
+                                if (isTest)
+                                {
+                                    Console.WriteLine(file.Name);
+                                }
+                                else
+                                {
+                                    file.Delete();
+                                }
+                            }
+                        } 
+                    }
+                } catch ( Exception e )
+                { Console.WriteLine(e.ToString()); }
+            }
+        }
+
+        internal bool areArgsValid(ParseArgs args)
+        {
+            if (args.Protokoll != "sftp" && args.Protokoll != "ftp")
+            {
+                Console.Write("Provide Valid Protokoll");
+                return false;
+            }
+
+            return true;
+        }
+
+        internal void writeTestData(string name)
+        {
+            string basePath = "TestData/";
+            using (SftpClient client = new SftpClient(_host, _userName, _password))
+            {
+                try { 
+                    client.Connect();
+                    client.WriteAllText(basePath + name + ".txt", "Whatever");
+
+                } catch (Exception e) { Console.WriteLine(e.ToString()); }  
+            }
+        }
+
+        internal void recursiveDeletion()
+        {
+            string basePath = "TestData/";
+            using (SftpClient client = new SftpClient(_host, _userName, _password))
+            {
+                try
+                {
+                    var directorys = new List<SftpFile>();
+                    client.Connect();
+                    var dirs = client.ListDirectory(basePath);
+                    foreach (var entry in dirs)
+                    {
+                        if (entry.IsDirectory)
+                        {
+                            directorys.Add(entry);
+                        }
+                        
+                    }
+
+                }
+                catch (Exception e) 
+                { Console.WriteLine(e.ToString()); }
+            }
+        }
+    }
+}
